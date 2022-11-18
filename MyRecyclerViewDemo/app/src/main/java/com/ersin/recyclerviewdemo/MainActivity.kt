06f04b77e1503bde2afc733f4f93a80a60214ea6
ersin98 <ersinyurtseven98@gmail.com>
2022-11-18 17:59:17 +0300

    UNDO gorevi

--- MyRecyclerViewDemo/app/src/main/java/com/ersin/recyclerviewdemo/MainActivity.kt
+++ MyRecyclerViewDemo/app/src/main/java/com/ersin/recyclerviewdemo/MainActivity.kt
@@ -1,27 +1,30 @@
 package com.ersin.recyclerviewdemo
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.accessibility.AccessibilityEventCompat.setAction
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ersin.recyclerviewdemo.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private var ItemList =
         arrayListOf(
-            Item("başlık","içerik1", UUID.randomUUID().toString()),
-            Item("başlık1","içerik2", UUID.randomUUID().toString()),
-            Item("başlık2","içerik3", UUID.randomUUID().toString()),
-            Item("başlık3","içerik4", UUID.randomUUID().toString()),
-            Item("başlık4","içerik5", UUID.randomUUID().toString()),
-            Item("başlık5","içerik6", UUID.randomUUID().toString()),
-            Item("başlık6","içerik8", UUID.randomUUID().toString()),
-            Item("başlık7","içerik9", UUID.randomUUID().toString()),
-            Item("başlık8","içerik10", UUID.randomUUID().toString()),
-            Item("başlık9","içerik11", UUID.randomUUID().toString()),
-            Item("başlık10","içerik12", UUID.randomUUID().toString()),
-            Item("başlık10","içerik13", UUID.randomUUID().toString()),
+            Item("What is Lorem Ipsum?","Lorem Ipsum is simply dummy text of the printing and typesetting industry.", UUID.randomUUID().toString()),
+            Item("header","Duis vitae erat congue, accumsan tortor et, pellentesque turpis.", UUID.randomUUID().toString()),
+            Item("123","Pellentesque sit amet odio venenatis, tempor mi id, viverra nisl.", UUID.randomUUID().toString()),
+            Item("deneme","Aliquam in lacus ut lacus commodo viverra non a est.", UUID.randomUUID().toString()),
+            Item("Sed vitae.","Fusce a nunc vitae lacus vestibulum commodo.", UUID.randomUUID().toString()),
+            Item("Aliquam vitae.","Sed tempus metus at aliquet molestie.", UUID.randomUUID().toString()),
+            Item("Fusce metus.","Etiam congue orci quis mi lobortis, a fringilla magna luctus.", UUID.randomUUID().toString()),
+            Item("In hac.","Nulla eleifend nisl sollicitudin augue vehicula, eget pellentesque sapien blandit.", UUID.randomUUID().toString()),
+            Item("Curabitur lorem.","Pellentesque commodo est non condimentum dignissim.", UUID.randomUUID().toString()),
+            Item("Integer quis.","Praesent porta justo sed metus dignissim, nec imperdiet quam facilisis.", UUID.randomUUID().toString()),
+            Item("Suspendisse molestie.","Integer facilisis lacus eu eros imperdiet, id volutpat risus varius.", UUID.randomUUID().toString()),
+            Item("In eleifend.","Duis eu diam auctor, porta quam at, accumsan quam.", UUID.randomUUID().toString()),
         )
 
     private lateinit var binding: ActivityMainBinding
@@ -34,13 +37,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
         val itemList= ItemList
         adapterList.updateList(itemList)
-        binding.buttonAdd.setOnClickListener { addItem() }
+        binding.buttonAdd.setOnClickListener {
+            val header = binding.editTextText2.text?.toString()
+            val text = binding.editTextText.text?.toString()
+            val newItem = Item(header, text,UUID.randomUUID().toString())
+            addItem(newItem)
+            binding.editTextText.text.clear()
+            binding.editTextText2.text.clear()
+        }
         binding.recyclerView.apply {
         layoutManager=LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL,false)
         adapter=adapterList
         }
-
-
 
 
     }
@@ -51,14 +59,18 @@ class MainActivity : AppCompatActivity() {
             layoutManager=LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL,false)
             adapter=adapterList
         }
+        val snack = Snackbar.make(binding.root, "APPLIED", Snackbar.LENGTH_SHORT)//LENGTH_INDEFINITE
+        snack.setAction("UNDO") {
+            addItem(item)
+        }
+        snack.show()
+
     }
 
 
-    private fun addItem(){
-        val header = binding.editTextText.text?.toString()
-        val text = binding.editTextText.text?.toString()
-        val newItem = Item(header, text,UUID.randomUUID().toString())
-        ItemList.add(newItem)
+    private fun addItem(item:Item){
+
+        ItemList.add(item)
         adapterList.updateList(ItemList)
         binding.recyclerView.apply {
             layoutManager=LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL,false)
